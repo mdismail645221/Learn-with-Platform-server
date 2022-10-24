a72002745bd09abce7f3dafaacdf68876ec5065b
mdismail645221 <mdismail645221@gmail.com>
2022-10-24 23:46:24 +0600

    folder structure seting

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req, res)=>{
+    res.send('Education learning is Running.')
+})
+
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`app listening on port ${port}`)
+})
