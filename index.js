9801ace795f72e6f9e8db49fe3bb0d12ae12aa09
mdismail645221 <mdismail645221@gmail.com>
2022-10-25 09:17:55 +0600

    4 id added

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ const app = express();
 app.use(cors());
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res)=>{
     res.send('Education learning is Running.')
+})
+
+app.get('/courses/', (req, res)=> {
+    res.send(courses)
 })
 
 
